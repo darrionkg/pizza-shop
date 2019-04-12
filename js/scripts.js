e6be4519a4e32258d74cdffee4b003e1906000c9
Darrion <darrionkg@gmail.com>
2019-04-12 16:23:36 -0700

    Add printOrder and fix click functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
 //Business Logic
-function Pizza(size) {
+function Pizza(size, cheese, crust, sauce) {
   this.size = size,
   this.toppings = [],
   this.cost = 0
+  this.cheese = cheese,
+  this.crust = crust,
+  this.sauce = sauce
 }
 
 Pizza.prototype.checkSize = function() {
@@ -17,14 +20,20 @@ Pizza.prototype.checkSize = function() {
   }
 }
 
-Pizza.prototype.addToToppings = function (topping) {
+Pizza.prototype.addToToppings = function(topping) {
   this.toppings.push(topping);
-  if(topping === "bacon" || topping === "grilledChicken") {
+  if(topping === "bacon" || topping === "grilled chicken") {
     this.cost += 0.50;
-  } else if(topping === "mushrooms" || topping === "bananaPeppers" || topping === "jalapenoPeppers") {
+  } else if(topping === "mushrooms" || topping === "banana peppers" || topping === "jalapeno peppers") {
     this.cost += 0.25;
   }
 };
+
+Pizza.prototype.checkCheeseCrust = function() {
+  if(this.crust === "Cheese Filled Crust") {
+    this.cost += 1.00;
+  }
+}
 
 function SidesOrDrinks() {
   this.sides = ["Breadsticks", "Coca Cola"],
@@ -44,7 +53,7 @@ SidesOrDrinks.prototype.addADrink = function() {
 
 function Order() {
   this.totalCost = 0,
-  this.listOfItems = [],
+  this.listOfPizzas = [],
   this.itemId = 0
 }
 
@@ -53,14 +62,32 @@ Order.prototype.updateId = function() {
 }
 
 Order.prototype.addPizza = function(pizza) {
-  this.listOfItems.push(pizza);
+  this.listOfPizzas.push(pizza);
   this.updateId();
   this.totalCost += pizza.cost;
 }
 
-Order.addSidesAndDrinks = function(sides) {
+Order.prototype.addSidesAndDrinks = function(sides) {
   this.updateId();
-  this.cost += sides.sidesCost;
+  this.totalCost += sides.sidesCost;
+}
+
+Order.prototype.printOrder = function(sides) {
+  var pizzaMessage = "";
+  for(var i = 0; i < this.listOfPizzas.length; i++) {
+    pizzaMessage += "1x " + this.listOfPizzas[i].size + " " + this.listOfPizzas[i].cheese + " Pizza:<br>-Toppings:<br>";
+    for(var j = 0; j < this.listOfPizzas[i].toppings.length; j++) {
+      pizzaMessage += "--" +this.listOfPizzas[i].toppings[j] + "<br>";
+    }
+
+  if(sides.numberOfSides[0] > 0) {
+    pizzaMessage += sides.numberOfSides[0] + "x 12 piece breadsticks<br>";
+  }
+  if(sides.numberOfSides[1] > 0) {
+    pizzaMessage += sides.numberOfSides[1] + "x 2 liter Coca Colas";
+  }
+    return pizzaMessage;
+  }
 }
 
 //User Interface Logic
@@ -69,8 +96,9 @@ var order = new Order();
 
 function updateItemCount() {
   var itemCount = order.itemId;
+  var total = order.totalCost.toFixed(2);
   $("#itemCount").text(itemCount);
-  $("#cost").text("Total - $" + order.totalCost)
+  $("#cost").text("Total - $" + total)
   $("#cost").show();
   $("#itemCount").show();
 }
@@ -88,12 +116,14 @@ $(document).ready(function() {
     var size = $("#size").val();
     var crust = $("#crust").val();
     var sauce = $("#sauce").val();
-    var newPizza = new Pizza(size);
+    var cheese = $("#cheese").val();
+    var newPizza = new Pizza(size, cheese, crust, sauce);
     $("input:checkbox[name=toppingChoice]:checked").each(function(){
       var topping = $(this).val();
       newPizza.addToToppings(topping);
     });
     newPizza.checkSize();
+    newPizza.checkCheeseCrust();
     order.addPizza(newPizza);
     updateActiveClass("#customPizza", "#sidesOrDrinks", "li#pizzaMenu", "li#sidesDrinksMenu");
     updateItemCount();
@@ -110,18 +140,25 @@ $(document).ready(function() {
   var sides = new SidesOrDrinks();
   $("#addDrinks").click(function() {
     sides.addADrink();
+    order.addSidesAndDrinks(sides);
+    updateItemCount();
   });
 
   $("#addSticks").click(function() {
     sides.addASide();
+    order.addSidesAndDrinks(sides);
+    updateItemCount();
   });
 
   $("#userOrder").submit(function(event) {
+    event.preventDefault();
     $("li#pizzaMenu").removeClass("active");
     $("li#sidesDrinksMenu").removeClass("active");
     $("#customPizza").hide();
     $("#sidesOrDrinks").hide();
+    updateItemCount();
+    var message = order.printOrder(sides);
+    $("#checkoutMessage").html(message);
     $("#checkout").show();
-    event.preventDefault();
   });
 });
