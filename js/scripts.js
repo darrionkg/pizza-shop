0162b2528053bf6611a53907b4439423c51d1979
Darrion <darrionkg@gmail.com>
2019-04-12 09:37:40 -0700

    Add pizza object with 2 prototype methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,45 @@
 //Business Logic
+function Pizza(size) {
+  this.size = size,
+  this.toppings = [],
+  this.cost = 0
+}
 
+Pizza.prototype.checkSize = function() {
+  if(this.size === "small") {
+    this.cost += 7.99;
+  } else if(this.size ==="medium") {
+    this.cost += 9.99;
+  } else if(this.size ==="large") {
+    this.cost += 11.99;
+  } else if(this.size ==="extraLarge") {
+     this.cost += 12.99;
+  }
+}
 
+Pizza.prototype.addToToppings = function (topping) {
+  this.toppings.push(topping);
+  if(topping === "bacon" || topping === "grilledChicken") {
+    this.cost += 0.50;
+  } else if(topping === "mushrooms" || topping === "bananaPeppers" || topping === "jalapenoPeppers") {
+    this.cost += 0.25;
+  }
+};
 
 //User Interface Logic
 $(document).ready(function() {
   $("#userOrder").submit(function(event) {
-
+    event.preventDefault();
+    var size = $("#size").val();
+    var crust = $("#crust").val();
+    var sauce = $("#sauce").val();
+    //var cheese = $("#cheese").val();
+    var toppings = [];
+    var newPizza = new Pizza(size);
+    $("input:checkbox[name=toppingChoice]:checked").each(function(){
+      var topping = $(this).val();
+      newPizza.addToToppings(topping);
+    });
+    newPizza.checkSize();
   });
 });
